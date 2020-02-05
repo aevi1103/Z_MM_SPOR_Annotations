2be12f0fa62b4443fdd7c8cbd3820f83f3f5df35
aevi1103 <aeviorontos@gmail.com>
2020-02-05 11:19:33 +0000

    to update fromatting

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -4,8 +4,49 @@ sap.ui.define([
 	"use strict";
 
 	return Controller.extend("com.federalmogul.Z_MM_SPOR.controller.App", {
+		
+		getResourceBundle : function () {
+			return this.getOwnerComponent().getModel("i18n");
+		},
+		
 		onInit: function () {
 
+		},
+		
+		formatStatusText: function (statusText) {
+			switch(statusText.toLowerCase()) {
+				case "d":
+					return this.getResourceBundle().getText("draft");
+				case "a":
+					return this.getResourceBundle().getText("awaitingApproval");
+				case "c":
+					return this.getResourceBundle().getText("completed");
+				case "p":
+					return this.getResourceBundle().getText("approved");
+				case "r":
+					return this.getResourceBundle().getText("rejected");
+				default:
+					return "";
+			}
+		},
+		
+		formatStatusRowHighlight: function (statusText) {
+			
+			switch(statusText.toLowerCase()) {
+				case "d":
+					return "Information";
+				case "a":
+					return "Warning";
+				case "c":
+					return "Success";
+				case "p":
+					return "Success";
+				case "r":
+					return "Error";
+				default:
+					return "";
+			}
 		}
+		
 	});
 });
